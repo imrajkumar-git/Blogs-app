5ee12bb499e330c20c47feceee8c2d0d17cfe409
Rajkumar <rajkumararyal0977@gmail.com>
2026-08-03 17:15:08 +0400

    all files added

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-insecure-secret-key-change-me")
 DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost,192.168.0.103").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "https://blogs-app-2-1vza.onrender.com").split(",")
 
 # ---------------------------------------------------------------------------
 # Applications
