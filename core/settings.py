97c8f435f4bd289e4114b2ef95d2d0ae8363aaf2
Rajkumar Aryal <80311912+imrajkumar-git@users.noreply.github.com>
2026-09-18 12:36:52 +0400

    Change email host user and password
    Updated email credentials for SMTP configuration.

--- core/settings.py
+++ core/settings.py
@@ -172,8 +172,8 @@ EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.smtp.
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
-EMAIL_HOST_USER = 'rajkumararyal0977@gmail.com'
-EMAIL_HOST_PASSWORD = 'pnierfurujtkbsbe'
+EMAIL_HOST_USER = 'aryalraj0000@gmail.com'
+EMAIL_HOST_PASSWORD = 'muzr jpdx jmwa boxd'
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default=EMAIL_HOST_USER)
 
 # If no Gmail creds are set, fall back to printing emails to the console
